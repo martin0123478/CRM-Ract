efb0883269b6feb75af3a97946aadefabc03700c
martin0123478 <martin_agui97@outlook.es>
2023-02-22 21:05:02 -0600

    Creando rutas y componente Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-
+import Layout from './components/Layout'
 import './index.css'
+import NuevoCliente from './pages/NuevoCliente'
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<h1>Inicio</h1>
+    element:<Layout/>,
+    children:[
+      {
+        index:true,
+        element:<h1>Inicio</h1>
+      },
+      {
+    path:'/clientes/nuevo',
+    element:<NuevoCliente/>
   }
+    ]
+  },
+  
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NuevoCliente() {
+  return (
+    <div>
+      Nuevo cliente
+    </div>
+  )
+}
+
+export default NuevoCliente
