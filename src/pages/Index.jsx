f7e3a6f19c266c018803dcd48a5593ac8d28e9cd
martin0123478 <martin_agui97@outlook.es>
2023-02-27 20:49:20 -0600

    Iterando el arreglo y mostrando los atos en una tabla

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Cklientes from "../components/Cklientes";
 
 export function loader(){
     const clientes = [
@@ -49,6 +50,30 @@ const Index = () => {
     <>
     <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
     <p className='mt-3'>Administra tus clientes</p>
+
+    {datos.length ? (
+        <table className="w-full shadow mt-5 table-auto">
+            <thead className="bg-blue-800 text-white">
+                <tr>
+                    <th className="p-2">Cliente</th>
+                    <th className="p-2">Contacto</th>
+                    <th className="p-2">Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {datos.map(cliente =>(
+                        <Cklientes
+                        cliente={cliente}
+                        key={cliente.id}
+                        />
+                    ))}
+                </tbody>
+
+            
+        </table>
+    ) : (
+        <p className="text-center mt-10">No hay clientes aun</p>
+    )}
     </>
   )
 }
